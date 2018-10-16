07c8bd9f1d707d75ec8871987f4e05891785db8e
Wilson Styres <wstyres@gmail.com>
2018-10-16 10:54:48 -0400

    Initial Commit

---
+++ carify/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  carify
+//
+//  Created by Wilson Styres on 10/16/18.
+//  Copyright © 2018 Wilson Styres. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
